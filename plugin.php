c4bfff4282e8c75cd851b77fa5724fdacd1059ab
Tarendai <contact@tomjn.com>
2014-02-17 16:46:20 +0000

    fix include ordering and add autoloader for the google APIs

--- plugin.php
+++ plugin.php
@@ -10,18 +10,21 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
 
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+	require __DIR__ . '/vendor/autoload.php';
+}
+
 require_once( 'php/cftp_analytics_factory.php');
-require_once( 'php/cftp_google_analytics_source.php');
 require_once( 'php/cftp_analytics_model.php');
 require_once( 'php/cftp_analytics_option_model.php');
 require_once( 'php/cftp_analytics.php');
 require_once( 'php/cftp_analytics_cron_task.php');
 require_once( 'php/cftp_analytics_settings_page.php');
 require_once( 'php/cftp_analytics_source.php');
+require_once( 'php/cftp_google_analytics_source.php');
 require_once( 'php/cftp_facebook_likes_source.php');
 require_once( 'php/cftp_facebook_shares_source.php');
 require_once( 'php/cftp_twitter_source.php');
-
 
 
 $factory = new cftp_analytics_factory();
